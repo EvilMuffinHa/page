56138ba87eabe50ec5ceaa49b228e75aef52c988
EvilMuffinHa <47578209+EvilMuffinHa@users.noreply.github.com>
2020-01-06 10:57:35 -0500

    Update page.py

--- page.py
+++ page.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for #ok
+#ok
 app = Flask(__name__)
-
+#ok
 @app.route('/')
 def index():
+  #page
   return render_template("index.html")
 
+#ok
 if __name__ == "__main__":
+  #page
     app.run(host="0.0.0.0", port=80)
+
+#ok
