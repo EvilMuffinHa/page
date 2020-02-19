0d222c6b117b2767872e7ca82c2a2122bee9ec05
EvilMuffinHa <47578209+EvilMuffinHa@users.noreply.github.com>
2020-02-19 14:22:02 -0500

    Update page.py

--- page.py
+++ page.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template("index.html")
+  return redirect("https://joshwang.me/r/pe", code=302)
+  return redirect("https://evilmuffinha.github.io/r/a.html", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
